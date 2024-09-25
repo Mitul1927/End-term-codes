21576d151c656ea53f46ef9cc0043e27c5639435
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2024-09-25 20:43:23 +0530

    Update reverse_a_linkedlist.java

--- reverse_a_linkedlist.java
+++ reverse_a_linkedlist.java
@@ -1,13 +1,21 @@
+/*Given a pointer to the head node of a doubly linked list, reverse the linked list*/
+
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode prev = null;
-        while(curr!=null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev=curr;
-            curr=next;
+    public ListNode reverseList(Node head) {
+        Node current = head;
+        Node temp = null;
+
+        // Traverse the list and swap next and prev for each node
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;  // Move to the next node in the original list
         }
-        return prev;
+
+        // If the list is not empty, set the new head to the last node
+        if (temp != null) {
+            head = temp.prev;  // temp.prev is the last node after reversal
+        }
     }
 }
